1e44bab6ed0058ff18fe8d90760b4ad81fb6f761
Evan <tech@neoscape.com>
2014-12-03 16:01:32 -0500

    refactor
    started comment cleaning, gentle code cleaning, organization

--- embUTCCardViews/embViewController.m
+++ embUTCCardViews/embViewController.m
@@ -13,13 +13,9 @@
 
 @interface embViewController ()
 {
-	CALayer *mlayer;
-	NSMutableArray *cards;
-	NSMutableArray *pauseTime;
-	CGFloat movieLength;
+	NSMutableArray *arr_HotspotInfoCards;
+	UIView *uiv_HotspotInfoCardContainer;
 	AVPlayer *player;
-	CGFloat prevXCardCenter;
-	UIView *cardContainer;
 }
 
 @property (nonatomic) NSTimer *myTimer;
@@ -28,14 +24,27 @@
 
 @implementation embViewController
 
+//----------------------------------------------------
+#pragma mark - view hierarchy
+//----------------------------------------------------
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 	
-	cards = [[NSMutableArray alloc] init];
-	pauseTime = [[NSMutableArray alloc] init];
+	// init arrays and create avplayer
+	arr_HotspotInfoCards = [[NSMutableArray alloc] init];
+	[self createAVPlayer];
 	
+	// start player and timer, create infocards
+	[self beginSequence];
+}
+
+//----------------------------------------------------
+#pragma mark - movie player
+//----------------------------------------------------
+-(void)createAVPlayer
+{
 	NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"05_HOTSPOT_A_COATED_STEEL_BELTS" withExtension:@"mov"];
 	AVURLAsset *asset = [AVURLAsset URLAssetWithURL:fileURL options:nil];
 	AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
@@ -47,31 +56,40 @@
 	
 	NSString *selectorAfterMovieFinished;
 	selectorAfterMovieFinished = @"playerItemLoop:";
-			
+	
 	[[NSNotificationCenter defaultCenter] addObserver:self
 											 selector:NSSelectorFromString(selectorAfterMovieFinished)
 												 name:AVPlayerItemDidPlayToEndTimeNotification
 											   object:[player currentItem]];
 	
 	[self.view.layer insertSublayer:playerLayer atIndex:0];
-	
-	[self timerandcards];
 }
 
--(void)timerandcards
+//----------------------------------------------------
+#pragma mark - init
+//----------------------------------------------------
+/*
+ start all info cards and movie in motion
+ */
+-(void)beginSequence
 {
 	[player play];
 	[self addTimerToPlayer];
 	[self createCards];
 }
-
+//----------------------------------------------------
+#pragma mark - info cards
+//----------------------------------------------------
+/*
+ create info cards from model
+ */
 -(void)createCards
 {
-	cardContainer = [[UIView alloc] initWithFrame:CGRectZero];
-	cardContainer.layer.backgroundColor = [UIColor clearColor].CGColor;
-	cardContainer.clipsToBounds = YES;
+	uiv_HotspotInfoCardContainer = [[UIView alloc] initWithFrame:CGRectZero];
+	uiv_HotspotInfoCardContainer.layer.backgroundColor = [UIColor clearColor].CGColor;
+	uiv_HotspotInfoCardContainer.clipsToBounds = YES;
 	
-	[self.view addSubview:cardContainer];
+	[self.view addSubview:uiv_HotspotInfoCardContainer];
 	
 	CGFloat tt = 0;
 	
@@ -80,8 +98,8 @@
 	card.text = @"Fireworks Graphical Command Interface/Incident Management Platform";
 	[card setFrame:CGRectMake(0, tt, 360, [self measureHeightOfUITextView:card.textView])];
 	card.alpha = 0;
-	[cards addObject:card];
-	[cardContainer addSubview:card];
+	[arr_HotspotInfoCards addObject:card];
+	[uiv_HotspotInfoCardContainer addSubview:card];
 	
 	tt += [self measureHeightOfUITextView:card.textView];
 	NSLog(@"%f",tt);
@@ -91,8 +109,8 @@
 	card.text = @"Monitor and control all necessary life safety network information through a graphic display of your facilities down to the detail of a single location";
 	[card setFrame:CGRectMake(0, tt, 360, [self measureHeightOfUITextView:card.textView])];
 	card.alpha = 0;
-	[cards addObject:card];
-	[cardContainer addSubview:card];
+	[arr_HotspotInfoCards addObject:card];
+	[uiv_HotspotInfoCardContainer addSubview:card];
 	
 	tt += [self measureHeightOfUITextView:card.textView];
 	NSLog(@"%f",tt);
@@ -102,8 +120,8 @@
 	card.text = @"Communicates other emergency events — weather, shelter in place, chemical spill, etc. — in lieu of an unsupervised intercom system";
 	[card setFrame:CGRectMake(0, tt, 360, [self measureHeightOfUITextView:card.textView])];
 	card.alpha = 0;
-	[cards addObject:card];
-	[cardContainer addSubview:card];
+	[arr_HotspotInfoCards addObject:card];
+	[uiv_HotspotInfoCardContainer addSubview:card];
 	
 	tt += [self measureHeightOfUITextView:card.textView];
 	
@@ -112,26 +130,60 @@
 	card.text = @"Optimum Start assures comfortable settings at time of occupancy based on building conditions and system capacity. Demand Reduction reduces energy use on a short-term basis in response to utility pricing, billing criteria, regional consumption, etc.";
 	[card setFrame:CGRectMake(0, tt, 360, [self measureHeightOfUITextView:card.textView])];
 	card.alpha = 0;
-	[cards addObject:card];
-	[cardContainer addSubview:card];
+	[arr_HotspotInfoCards addObject:card];
+	[uiv_HotspotInfoCardContainer addSubview:card];
 	
 	tt += [self measureHeightOfUITextView:card.textView];
 	NSLog(@"%f",tt);
 	
 	// update container frame now that we know the heights
-	cardContainer.frame = CGRectMake(120, 200, 360, tt);
+	uiv_HotspotInfoCardContainer.frame = CGRectMake(120, 200, 360, tt);
 
 }
 
+-(void)removeCards
+{
+	NSInteger i = 0;
+	
+	for (embUiViewCard *card in arr_HotspotInfoCards)
+	{
+		
+		UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction;
+		[UIView animateWithDuration:.2 delay:((0.05 * i) + 0.2) options:options
+						 animations:^{
+							 card.alpha = 0.0;
+						 }
+						 completion:^(BOOL finished){
+							 [arr_HotspotInfoCards removeObject:card];
+						 }];
+		i += 1;
+	}
+}
+
+
+//----------------------------------------------------
+#pragma mark - movie player
+//----------------------------------------------------
+/*
+ movie in the background
+ */
 -(void)playerItemLoop:(NSNotification *)notification
 {
 	AVPlayerItem *p = [notification object];
 	[p seekToTime:kCMTimeZero];
 	
 		
-	[self timerandcards];
+	[self beginSequence];
 }
 
+//----------------------------------------------------
+#pragma mark - utilties
+//----------------------------------------------------
+#pragma mark  textview height
+//----------------------------------------------------
+/*
+ calculate height of
+ */
 - (CGFloat)measureHeightOfUITextView:(UITextView *)textView
 {
     if ([textView respondsToSelector:@selector(snapshotViewAfterScreenUpdates:)])
@@ -192,43 +244,13 @@
     }
 }
 
-//- (CGFloat)heightForTextView:(embUiViewCard*)card containingString:(NSString*)string
-//{
-//    float horizontalPadding = 24;
-//    float verticalPadding = 15;
-//	// float widthOfTextView = card.textView.contentSize.width - horizontalPadding;
-//	//float height = [string sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(360, 999999.0f) lineBreakMode:NSLineBreakByWordWrapping].height + verticalPadding;
-//	CGFloat fontSize = 17;
-//	CGRect r = [card.text boundingRectWithSize:CGSizeMake(360, 0)
-//								  options:NSStringDrawingUsesDeviceMetrics
-//							   attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:fontSize]}
-//								  context:nil];
-//	
-//	float height = r.size.height + verticalPadding;
-//	
-//	NSLog(@"fontSize = %f\tbounds = (%f x %f)",
-//		  fontSize,
-//		  r.size.width,
-//		  r.size.height);
-//	
-//    return height;
-//}
-//
-//-(void)textHeight:(NSString*)text
-//{
-//	for (NSNumber *n in @[@(0.0f), @(0.0f), @(17.0f)]) {
-//		CGFloat fontSize = [n floatValue];
-//		CGRect r = [text boundingRectWithSize:CGSizeMake(200, 0)
-//										   options:NSStringDrawingUsesLineFragmentOrigin
-//										attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:fontSize]}
-//										   context:nil];
-////		NSLog(@"fontSize = %f\tbounds = (%f x %f)",
-////			  fontSize,
-////			  r.size.width,
-////			  r.size.height);
-//	}
-//
-//}
+//----------------------------------------------------
+#pragma mark - timer
+//----------------------------------------------------
+/*
+ keeps track of number seconds elapsed 
+ to help sync which card to reveal next
+ */
 
 -(void)addTimerToPlayer
 {
@@ -245,72 +267,53 @@
 
 	NSLog(@"start timer");
 }
-
+//----------------------------------------------------
+#pragma mark  utility seconds
+//----------------------------------------------------
+/*
+ as each second passes check if the seconds
+ match the reveal delay
+*/
 - (void)displayMyCurrentTime:(NSTimer *)timer
 {
+	CGFloat movieLength = CMTimeGetSeconds([player currentTime]);
 	
-	//NSLog(@"displayMyCurrentTime");
-	
-//  float dur = CMTimeGetSeconds([player currentTime]);
-//  float durInMiliSec = 1000.0 * dur;
-//  if (durInMiliSec > 0.0) NSLog(@"CMT %0.3f", durInMiliSec);
-
-	movieLength = CMTimeGetSeconds([player currentTime]);
-//  if (movieLength > 0.0) NSLog(@"CMT %f Seconds", movieLength);
-	
-	//embUiViewCard *ccard;
 	int y = movieLength;
 	NSLog(@"seconds %i",y);
 	
+//TODO: pass the time to remove from the model
 	if (y == 13) {
 		[self removeCards];
 	}
 	
+	// check if the seconds match the reveal delay
 	[self indexOfCardToReveal:y];
 }
 
+//----------------------------------------------------
+#pragma mark - find card that should appear
+//----------------------------------------------------
+
+// find card and remove
 -(void)indexOfCardToReveal:(int)index
 {
-	for (embUiViewCard *card in cards)
+	for (embUiViewCard *card in arr_HotspotInfoCards)
 	{
 		if (card.delay == index) {
-			embUiViewCard *ccard = cards[[cards indexOfObject:card]];
+			embUiViewCard *ccard = arr_HotspotInfoCards[[arr_HotspotInfoCards indexOfObject:card]];
 			[self revealCard:ccard afterDelay:0];
 		}
 	}
 	
-	/*
-	switch (index) {
-		case 3:
-			ccard = cards[0];
-			
-			break;
-			
-		case 5:
-			ccard = cards[1];
-			break;
-			
-		case 7:
-			ccard = cards[2];
-			break;
-			
-		case 11:
-			ccard = cards[3];
-			break;
-			
-		default:
-			break;
-	}
-
-	[self revealCard:ccard afterDelay:0];
-	 */
 }
 
+//----------------------------------------------------
+#pragma mark reveal card
+//----------------------------------------------------
+
+// reveal card
 -(void)revealCard:(embUiViewCard*)card afterDelay:(CGFloat)delay
 {
-	
-	//__block CGFloat newX;
-	
 	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 				
 		card.alpha = 1.0;
@@ -322,28 +325,10 @@
 		// get index of card to know whether
 		// to animate it bouncing/falling
 		
-		NSUInteger index = [cards indexOfObject:card];
-		NSLog(@"%i",index);
+		NSUInteger index = [arr_HotspotInfoCards indexOfObject:card];
 		
 		embUiViewCard *ccard;
-		ccard =  [cards objectAtIndex:index];
-		
-//if (index == 0)
-//{
-//	NSLog(@"asdas");
-//	int previndex = (int)index - 1;
-//	NSLog(@"%i",previndex);
-//	
-//	ccard =  [cards objectAtIndex:index];
-//	//prevXCardCenter = ccard.center.y;
-//	
-//	CGRect aRect = CGRectMake(280., 235, ccard.frame.size.width , ccard.frame.size.height);
-//	NSLog(@"aRect: %@", NSStringFromCGRect(aRect));
-//
-//}
-		
-		CGFloat startValueX = card.frame.origin.x;
-		CGFloat startValueY = card.frame.origin.y;
+		ccard =  [arr_HotspotInfoCards objectAtIndex:index];
 		
 		CGFloat startPointX = card.center.x;
 		CGFloat startPointY = card.center.y;
@@ -352,47 +337,10 @@
 		
 
 		UIView *maskView = [[UIView alloc] initWithFrame:CGRectMake(card.frame.origin.x, card.frame.origin.y, 360, card.frame.size.height+20)];
-		
-		// ...position maskView...
-		//maskView.layer.position = CGPointMake(CGRectGetMidX([self.view bounds]), CGRectGetMidY([self.view bounds]));
-		
 		maskView.layer.backgroundColor = [UIColor clearColor].CGColor;
-		
 		maskView.clipsToBounds = YES;
-
 		[maskView addSubview:card];
-		
-		[cardContainer addSubview:maskView];
-
-
-		
-//		// MASK WORKS
-//		CALayer *mask = [CALayer layer];
-//		mask.contents = (id)[[UIImage imageNamed:@"card_mask.png"] CGImage];
-//		
-////		if (index == 0) {
-////			mask.frame = CGRectMake(0, 0, 0, 0);
-////			mask.anchorPoint = CGPointMake(0, 0);
-////		} else {
-//			mask.frame = CGRectMake(0, startValueY, 360, card.frame.size.height);
-////			mask.anchorPoint = CGPointMake(.5, 0);
-////		}
-//		
-//		card.layer.mask = mask;
-//		
-//		CGRect oldBounds = mask.bounds;
-//		CGRect newBounds = card.bounds;
-//		
-//		CABasicAnimation* revealAnimation = [CABasicAnimation animationWithKeyPath:@"bounds"];
-//		revealAnimation.fromValue = [NSValue valueWithCGRect:CGRectMake(0, 0, 360, card.frame.size.height)];
-//		revealAnimation.toValue = [NSValue valueWithCGRect:newBounds];
-//		revealAnimation.duration = 0.33;
-//		
-//		// Update the bounds so the layer doesn't snap back when the animation completes.
-//		mask.bounds = newBounds;
-//		
-//		[mask addAnimation:revealAnimation forKey:@"revealAnimation"];
-//		// END MASK WORKS
+		[uiv_HotspotInfoCardContainer addSubview:maskView];
 		
 		NSArray *values;
 		
@@ -414,67 +362,8 @@
 		[card.layer addAnimation:anim forKey:@"position"];
 
 		[card setCenter:CGPointMake(startPointX, card.frame.size.height/2)];
-		
-		//card.layer.position = CGPointMake(280, 235+prevXCardCenter);
-		
+				
 		NSLog(@"rect1: %@", NSStringFromCGRect(card.frame));
-		
-		
-		
-// KIND OF WORKS
-//				int index = [cards indexOfObject:card];
-//
-//				newX = index*95;
-//		
-//			
-//				CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-//				
-//		//NSArray *times = [NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.33], [NSNumber numberWithFloat:0.5], [NSNumber numberWithFloat:1.0], nil];
-//				//		[anim setKeyTimes:times];
-//			NSArray *values;
-//			if (index == 0) {
-//				values = [NSArray arrayWithObjects:[NSValue valueWithCGPoint:CGPointMake(280., 200.)],
-//						  [NSValue valueWithCGPoint:CGPointMake(280., 235+newX)], nil];
-//			} else {
-//				values = [NSArray arrayWithObjects:[NSValue valueWithCGPoint:CGPointMake(280., 235.+newX)],
-//						  [NSValue valueWithCGPoint:CGPointMake(280., 235+newX)], nil];
-//			}
-//			
-//			[anim setValues:values];
-//			[anim setDuration:.33]; //seconds
-//			//[anim setCalculationMode:kCAAnimationCubicPaced];
-//			
-//			[card.layer addAnimation:anim forKey:@"position"];
-// END KIND OF WORKS
-
-		
-// MASK WORKS
-//			CALayer *mask = [CALayer layer];
-//			mask.contents = (id)[[UIImage imageNamed:@"card_mask.png"] CGImage];
-//			
-//			if (index == 0) {
-//				mask.frame = CGRectMake(0, 0, 0, 0);
-//				mask.anchorPoint = CGPointMake(0, 0);
-//			} else {
-//				mask.frame = CGRectMake(0, 0, 360, 0);
-//				mask.anchorPoint = CGPointMake(.5, 0);
-//			}
-//			
-//			card.layer.mask = mask;
-//			
-//			CGRect oldBounds = mask.bounds;
-//			CGRect newBounds = card.bounds;
-//			
-//			CABasicAnimation* revealAnimation = [CABasicAnimation animationWithKeyPath:@"bounds"];
-//			revealAnimation.fromValue = [NSValue valueWithCGRect:oldBounds];
-//			revealAnimation.toValue = [NSValue valueWithCGRect:newBounds];
-//			revealAnimation.duration = 0.33;
-//			
-//			// Update the bounds so the layer doesn't snap back when the animation completes.
-//			mask.bounds = newBounds;
-//			
-//			[mask addAnimation:revealAnimation forKey:@"revealAnimation"];
-// END MASK WORKS
 		
 		CALayer *mask = [CALayer layer];
 		mask.contents = (id)[[UIImage imageNamed:@"card_mask.png"] CGImage];
@@ -500,14 +389,17 @@
 			
 		}
 
-		
-		//[card setCenter:CGPointMake(280., 235)];
-		//[card setCenter:CGPointMake(280., 235+card.center.y+card.frame.size.height)];
-
-		//NSLog(@"rect1: %@", NSStringFromCGRect(card.frame));
-
 	});
 }
+
+//----------------------------------------------------
+#pragma mark - pause card animations
+//----------------------------------------------------
+/*
+ actions for pausing and resuming animations
+ */
+
+
 
 - (IBAction)pauseAnimation
 {
@@ -515,7 +407,7 @@
 	
 	[player pause];
 		
-	for (embUiViewCard *card in cards)
+	for (embUiViewCard *card in arr_HotspotInfoCards)
 	{
 		[self pauseLayer:card.layer];
 	}
@@ -529,44 +421,17 @@
 		[_myTimer pauseOrResume];
 	}
 	
-	for (embUiViewCard *card in cards)
+	for (embUiViewCard *card in arr_HotspotInfoCards)
 	{
 		[self resumeLayer:card.layer];
 	}
 }
 
--(void)removeCards
-{
-	NSInteger i = 0;
-
-	for (embUiViewCard *card in cards)
-	{
-		
-		UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction;
-		[UIView animateWithDuration:.2 delay:((0.05 * i) + 0.2) options:options
-						 animations:^{
-							 card.alpha = 0.0;
-						 }
-						 completion:^(BOOL finished){
-							 [cards removeObject:card];
-						 }];
-	
-		i += 1;
-	}
-}
-
-
 -(void)pauseLayer:(CALayer*)layer
 {
-    
-		CFTimeInterval pausedTime = [layer convertTime:CACurrentMediaTime() fromLayer:nil];
-		layer.speed = 0.0;
-		layer.timeOffset = pausedTime;
-	
-	CGFloat t = pausedTime;
-	
-	[pauseTime addObject:[NSNumber numberWithFloat:t]];
-
+	CFTimeInterval pausedTime = [layer convertTime:CACurrentMediaTime() fromLayer:nil];
+	layer.speed = 0.0;
+	layer.timeOffset = pausedTime;
 }
 
 -(void)resumeLayer:(CALayer*)layer
@@ -579,6 +444,9 @@
     layer.beginTime = timeSincePause;
 }
 
+//----------------------------------------------------
+#pragma mark - boilerplate
+//----------------------------------------------------
 
 - (void)didReceiveMemoryWarning
 {
